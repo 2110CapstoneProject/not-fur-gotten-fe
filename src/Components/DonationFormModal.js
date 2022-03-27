48ee51ed7232535efbb9abd51dcc569c3089f199
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-27 13:14:46 -0700

    Add classNames and attributes to JSX elements

--- src/Components/DonationFormModal.js
+++ src/Components/DonationFormModal.js
@@ -4,28 +4,36 @@ import '../Styles/DonationFormModal.scss';
 const DonationFormModal = () => {
   return (
     <div className="modal">
-      <form>
-        <h2>Donation Form</h2>
-        <section>
-          <h3>Owner Information</h3>
-          <div>
-            <input type="text" />
-            <input type="text" />
+      <form className="donation-form">
+        <h2 className="form-title">Donation Form</h2>
+        <section className="owner-info-container">
+          <h3 className="form-subtitle">Owner Information</h3>
+          <div className="owner-name-container">
+            <input type="text" placeholder="First Name"/>
+            <input type="text" placeholder="Last Name"/>
           </div>
-          <textarea />
+          <textarea
+            placeholder="Tell potential adopters about yourself..."
+            rows="10"
+            cols="10"
+          />
         </section>
-        <section>
-          <h3>Pet Information</h3>
+        <section className="pet-info-container">
+          <h3 className="form-subtitle">Pet Information</h3>
           <div>
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+            <input type="text" placeholder="Pet Name"/>
+            <input type="text" placeholder="Age"/>
+            <input type="text" placeholder="Species"/>
+            <input type="text" placeholder="Gender"/>
           </div>
-          <textarea />
-          <input type="files" />
+          <textarea
+            placeholder="Tell potential adopters about your pet..."
+            rows="10"
+            cols="10"
+          />
+          <input type="file" />
         </section>
-        <button>Submit</button>
+        <button className="submit-button">Submit</button>
       </form>
     </div>
   );
