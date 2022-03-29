1da1ea6354903544ab5a46de55f94babda6fec84
Christine Rowland <fordo29@gmail.com>
2022-03-29 17:13:38 -0600

    Import and implement the Header component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../Styles/Home.scss';
 import PetContainer from './PetContainer';
+import Header from './Header';
 
 const Home = () => {
   return (
     <div>
       <section className='upperSection'>
+        <div>
+          <Header />
+        </div>
         <h1>Find a forever home for your best friend</h1>
       </section>
       <PetContainer />
