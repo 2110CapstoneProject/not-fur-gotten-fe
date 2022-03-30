ddfefeaacbcd494697a6ce670225d2559ebd1cb2
Alexis Newby <alexis.newby@gmail.com>
2022-03-30 14:55:51 -0600

    Set up query and pass data as props

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import PetContainer from './PetContainer';
 import Header from './Header';
+import { useQuery, gql } from '@apollo/client';
 import '../Styles/Home.scss';
 
+const GET_PETS = gql`
+  {
+    getAllPets {
+      id
+      name
+    }
+  }
+`
 
 const Home = () => {
+
+  const { error, loading, data } = useQuery(GET_PETS)
+
   return (
     <div>
       <section className='upperSection'>
         <Header />
         <h1>Find a forever home for your best friend</h1>
       </section>
-      <PetContainer />
+      <PetContainer pets={data}/>
     </div>
   )
 }
