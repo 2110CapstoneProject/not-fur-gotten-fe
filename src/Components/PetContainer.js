c665f7f4267dc741575f8fc8d5022e2d6d9acb5f
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-04-02 08:32:25 -0700

    Remove Link component

--- src/Components/PetContainer.js
+++ src/Components/PetContainer.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import PetCard from './PetCard';
-import { Link } from 'react-router-dom';
 import '../Styles/PetContainer.scss';
 
 
 const PetContainer = ({ pets }) => {
   const petCards = pets.map(pet => {
     return (
-      <Link to={`pet/${pet.id}`} key={pet.id}>
-        <PetCard
-          id={pet.id}
-          name={pet.name}
-        />
-      </Link>
+      <PetCard
+        id={pet.id}
+        name={pet.name}
+      />
     )
   });
 
