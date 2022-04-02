9fe7ab752caa66b7ce24b556b4b361ee232a3697
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-04-01 18:09:10 -0700

    Implement props to build out pet card and finalize styling

--- src/Components/PetCard.js
+++ src/Components/PetCard.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../Styles/PetCard.scss';
 
-
-const PetCard = () => {
+const PetCard = ({ id, name }) => {
   return (
-    <div className='pet-card'>
-      <h3>Pet Card</h3>
-      <h4>Pet Name</h4>
-
+    <div className="pet-card">
+      <img className="pet-image" src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1617&q=80" alt="{name}"/>
+      <h3 className="pet-card-name">{name}</h3>
     </div>
   )
 }
