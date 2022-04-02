3cb3f33d6a364d9706456633ea4c5f0b96457ae9
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-04-02 10:53:57 -0700

    Build tests for Home page view
    Created the spec file for the Home page component. Added tests to check
    that all expected components/jsx elements appear on the page. I also
    created a fixture file to with dummy data to stub the intercept with.

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,75 @@
+import { aliasQuery, hasOperationName } from '../utils/graphql-test-utils';
+
+describe('Not Fur-gotten Home Page User Flow', () => {
+  // beforeEach(() => {
+  //   cy.intercept('POST', 'https://not-fur-gotten-be.herokuapp.com/graphql', (req) => {
+  //     const { body } = req
+  //     if (hasOperationName(req, 'getAllPets')) {
+  //       req.alias = 'gqlgetAllPetsQuery';
+  //       req.reply((res) => {
+  //         res.body.data.getAllPets = [
+  //           {
+  //             id: 1,
+  //             name: "Bongo"
+  //           },
+  //           {
+  //             id: 2,
+  //             name: "Karl"
+  //           },
+  //           {
+  //             id: 3,
+  //             name: "Sparky"
+  //           }
+  //         ];
+  //       });
+  //     }
+  //   });
+  // });
+
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: 'https://not-fur-gotten-be.herokuapp.com/graphql',
+        headers: {
+          'x-gql-operation-name': 'getAllPets',
+        }
+      },
+      {
+        fixture: 'petsForHomePage.json'
+      }
+    ).as('getAllPets');
+  });
+
+  it('should display all elements on the Home page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.wait('@getAllPets')
+      .get('h1')
+      .contains('Not Fur-gotten')
+      .get('p')
+      .contains('Back to Home Page')
+      .get('.hero-banner-text')
+      .contains('Find a forever home for your best friend.')
+      .get('.rehome-button')
+      .contains('Rehome Your Pet')
+      .get('.home-page-subheader')
+      .contains('Pets Available For Adoption')
+      .get('.pet-container').children()
+      .should('have.length', 5);
+  });
+
+  it('should be able to navigate to the donation form from the home page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.wait('@getAllPets')
+      .get('.rehome-button').click();
+  })
+
+  it('should be able to navigate to a particular pet page on click', () => {
+    cy.visit('http://localhost:3000/')
+    cy.wait('@getAllPets')
+      .get('a[href="/pet/1"]').click()
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/pet/1');
+    })
+  })
+});
