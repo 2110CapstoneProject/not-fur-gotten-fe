8afb23859afc2851516c326007da8887689df0eb
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-03 10:53:01 -0600

    Update application card with passed down props containing queried data

--- src/Components/ApplicationCard.js
+++ src/Components/ApplicationCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../Styles/ApplicationCard.scss';
 
-const ApplicationCard = () => {
+const ApplicationCard = ({name, email, description}) => {
   return (
     <section className='application-card'>
       <div className='applicant-name'>
-        <h3>Loki Lucifer</h3>
-        <p className='email'>catsmeow@gmail.com</p>
+        <h3>{name}</h3>
+        <p className='email'>{email}</p>
       </div>
       <div className='applicant-details'>
         <p className='intro-label'>Applicant Introduction:</p>
-        <p className='intro'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. tetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className='intro'>{description}</p>
       </div>
     </section>
   )
