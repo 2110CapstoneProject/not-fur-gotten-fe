5e90c6deb490b6a6d1e07ba73240eb54d85bb4e8
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-03 11:30:37 -0600

    Finalize testing for pet applications page

--- cypress/integration/pet_applications_spec.js
+++ cypress/integration/pet_applications_spec.js
@@ -1,20 +1,37 @@
 describe('Pet Applications Component', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: 'https://not-fur-gotten-be.herokuapp.com/graphql',
+        headers: {
+          'x-gql-operation-name': 'getPetById',
+        }
+      },
+      {
+        fixture: 'petApplicationsPage.json'
+      }
+    ).as('singlePetApplications');
+  });
+
   it('Should display the nav bar, title, and applications for a pet', () => {
     cy.visit('http://localhost:3000/pet/1/applications')
+    cy.wait('@singlePetApplications')
       .get('h1')
       .contains('Not Fur-gotten')
       .get('p')
       .contains('Back to Home Page')
-      .get('h3')
+      .get('.application-title')
+      .contains('Applications for Godzilla')
       .get('.application-container')
       .children()
-      .should('have.length', 3)
-      .contains('Loki Lucifer')
+      .should('have.length', 2)
+      .contains('Lenny')
       .get('p')
-      .contains('catsmeow@gmail.com')
+      .contains('lenny@gmail.com')
       .get('p')
       .contains('Applicant Introduction:')
       .get('p')
-      .contains('Lorem ipsum dolor')
+      .contains('I love cats')
   })
 })
