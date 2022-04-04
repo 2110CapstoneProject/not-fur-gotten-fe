f73c37458af9ee03c66db2720ea088255061c5a3
Stevie Ambroise <s.ambroise@protonmail.com>
2022-04-04 13:25:29 -0600

    Pass down image prop to img src field

--- src/Components/PetCard.js
+++ src/Components/PetCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../Styles/PetCard.scss';
 
-const PetCard = ({ id, name }) => {
+const PetCard = ({ id, name, image }) => {
   const linkStyle = {
     textDecoration: 'none',
     color: 'black',
@@ -10,7 +10,7 @@ const PetCard = ({ id, name }) => {
   return (
     <div className="pet-card">
       <Link style={linkStyle} to={`pet/${id}`}>
-        <img className="pet-image" src="https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1617&q=80" alt="{name}"/>
+        <img className="pet-image" src={image} alt="{name}"/>
         <h3 className="pet-card-name">{name}</h3>
       </Link>
     </div>
