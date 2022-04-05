3860ec92cd22bdd773627cedd4005a1dd19cf98d
Christine Rowland <fordo29@gmail.com>
2022-04-05 16:39:22 -0600

    Update header testing in Error page

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -1,10 +1,7 @@
 describe('Error Component', () => {
   it('should display the nav bar and message', () => {
     cy.visit('http://localhost:3000/error')
-      .get('h1')
-      .contains('Not Fur-gotten')
-      .get('p')
-      .contains('Back to Home Page')
+      .get('header')
       .get('h1')
       .contains('Error Message TBD')
   })
