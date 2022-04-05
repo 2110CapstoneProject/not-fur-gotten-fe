1c229ab4e340e9a9b553b15db76b335a65f677de
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-04-05 10:20:56 -0700

    Merge donation_form_spec into home_page_spec

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -9,6 +9,20 @@ describe('Not Fur-gotten Home Page User Flow', () => {
         }
       },
       {
+        fixture: 'newPetHomePage.json'
+      }
+    ).as('newPetAdded');
+
+    cy.intercept(
+      {
+        method: 'POST',
+        url: 'https://not-fur-gotten-be.herokuapp.com/graphql',
+        headers: {
+          'x-gql-operation-name': 'getAllPets',
+        },
+        times: 1
+      },
+      {
         fixture: 'petsForHomePage.json'
       }
     ).as('getAllPets');
@@ -34,8 +48,81 @@ describe('Not Fur-gotten Home Page User Flow', () => {
   it('should be able to navigate to the donation form from the home page', () => {
     cy.visit('http://localhost:3000/')
     cy.wait('@getAllPets')
-      .get('.rehome-button').click();
-  })
+      .get('.rehome-button').click()
+      .get('.modal')
+      .get('.form-title')
+      .should('have.text', 'Donation Form')
+      .get('h3')
+      .contains("Owner Information")
+      .get('label')
+      .contains('Full Name')
+      .get('label')
+      .contains('Email')
+      .get('label')
+      .contains('Owner Story')
+      .get('h3')
+      .contains('Pet Information')
+      .get('label')
+      .contains('Pet Name')
+      .get('label')
+      .contains('Age (years)')
+      .get('label')
+      .contains('Type')
+      .get('label')
+      .contains('Gender')
+      .get('label')
+      .contains('Description')
+      .get('label')
+      .contains('Add an image of your pet:')
+      .get('[data-testid="image-upload"]')
+      .should('exist')
+      .get('.upload-button')
+      .contains('Upload')
+      .get('button')
+      .contains('Submit')
+  });
+
+  it('should be able to fill out the form and submit a pet for adoption', () => {
+    cy.intercept('POST', 'https://api.cloudinary.com/v1_1/dzfyvxcwi/upload', { fixture: 'urlForPet.json' }).as('restfulPOST')
+    cy.intercept(
+      {
+        method: 'POST',
+        url: 'https://not-fur-gotten-be.herokuapp.com/graphql',
+        headers: {
+          'x-gql-operation-name': 'createPet',
+        }
+      },
+      {
+        fixture: 'petSubmission.json'
+      }
+    ).as('petSubmission');
+    cy.visit('http://localhost:3000/')
+    cy.wait('@getAllPets')
+      .get('.rehome-button').click()
+      .get('input[id="ownerName"]')
+      .type('Jack Sparrow')
+      .get('input[id="ownerEmail"]')
+      .type('captainsparrow@blackpearl.net')
+      .get('textarea[id="ownerStory"]')
+      .type('I am a seafairing pirate, and need a new home for my undead monkey.')
+      .get('input[id="petName"]')
+      .type('Jack')
+      .get('input[id="age"]')
+      .type('7')
+      .get('input[id="type"]')
+      .type('Monkey')
+      .get('select[id="gender"]')
+      .select('Male')
+      .get('textarea[id="petDescription"]')
+      .type('He\'s a little trickster. Keep an eye on your treasures, he has sticky fingers!')
+      .get('input[type="file"]')
+      .selectFile('src/test-file-image.jpg')
+      .get('.upload-button').click()
+      .wait('@restfulPOST')
+      .get('.submit-button').click()
+      .wait(['@newPetAdded', '@petSubmission'])
+      .get('img[alt="Jack"]')
+  });
 
   it('should be able to navigate to a particular pet page on click', () => {
     cy.visit('http://localhost:3000/')
@@ -43,6 +130,6 @@ describe('Not Fur-gotten Home Page User Flow', () => {
       .get('a[href="/pet/1"]').click()
     cy.location().should((loc) => {
       expect(loc.href).to.eq('http://localhost:3000/pet/1');
-    })
-  })
+    });
+  });
 });
