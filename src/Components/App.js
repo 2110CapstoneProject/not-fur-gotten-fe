d294b0316aade545bac9d8f376041c424f5ab0d7
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-04-04 18:11:02 -0700

    Remove deprecated Route components

--- src/Components/App.js
+++ src/Components/App.js
@@ -5,19 +5,15 @@ import Home from './Home';
 import Pet from './Pet';
 import PetApplications from './PetApplications';
 import Error from './Error';
-import ApplicationFormModal from './ApplicationFormModal';
-import DonationFormModal from './DonationFormModal';
 
 function App() {
-  
+
   return (
     <div className="App">
       <Routes>
         <Route path="/"  element={<Home />} />
         <Route path="pet/:id" element={<Pet />} />
         <Route path="pet/:id/applications" element={<PetApplications />} />
-        <Route path='application' element={<ApplicationFormModal />} />
-        <Route path='donation' element={<DonationFormModal />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </div>
