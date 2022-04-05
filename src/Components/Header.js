e0bf9e88e80b89feb93df39ca3ca168d39f285a7
Christine Rowland <fordo29@gmail.com>
2022-04-05 12:34:09 -0600

    Import use location hook

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link, useLocation } from 'react-router-dom';
 import '../Styles/Header.scss';
 
 const Header = () => {
-  
+  let location = useLocation();
+  let goBackLink;
+  console.log(location)
+
+  // if (!location) {
+  //   goBackLink = null
+  // } else {
+  //   goBackLink = <Link><p>Back to Home Page</p></Link>
+  // }
+
   return (
     <header className="header">
-      <h1>Not Fur-gotten</h1>
+      <Link to="/">
+        <h1>Not Fur-gotten</h1>
+      </Link>
       <p>Back to Home Page</p>
     </header>
   )
