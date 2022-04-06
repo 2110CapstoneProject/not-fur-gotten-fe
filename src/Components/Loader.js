2f467f5276333bffcacfcc04a0563e9fd535ce7f
Alexis Newby <alexis.newby@gmail.com>
2022-04-06 10:05:57 -0600

    Build Loader component- we did not create but will credit

---
+++ src/Components/Loader.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import '../Styles/Loader.scss';
+
+const Loader = () => {
+  return (
+  <div className='loader-image'>
+    <div className="paw-print-1">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>
+      
+    <div className="paw-print-2">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>    
+      
+    <div className="paw-print-3">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>    
+      
+    <div className="paw-print-4">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>
+      
+    <div className="paw-print-5">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>
+      
+    <div className="paw-print-6">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>
+      
+    <div class="paw-print-7">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>
+
+    <div className="paw-print-8">
+      <div className="pad large"></div>
+      <div className="pad small-1"></div>
+      <div className="pad small-2"></div>
+      <div className="pad small-3"></div>
+      <div className="pad small-4"></div>
+    </div>
+
+    <h1 className='loading-word'>LOADING...</h1>
+  </div>
+  )
+}
+
+export default Loader
