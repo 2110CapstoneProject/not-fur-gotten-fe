59cc0b0db820e4722d0d85c62ad52c6b8ce4ae3b
Christine Rowland <fordo29@gmail.com>
2022-04-05 19:13:59 -0600

    Add Error handling testing & account for sad path
    We added Error handling testing and accounted for sad path
    testing for home page and pet applications. Then we made a fixture
    to stub getAllPets query to return no pets.

--- cypress/integration/error_spec.js
+++ cypress/integration/error_spec.js
@@ -1,8 +1,48 @@
 describe('Error Component', () => {
-  it('should display the nav bar and message', () => {
+  beforeEach(() => {
+    cy.intercept(
+      {
+        method: 'POST',
+        url: 'https://not-fur-gotten-be.herokuapp.com/graphql',
+        headers: {
+          'x-gql-operation-name': 'getAllPets',
+        },
+        times: 1
+      },
+      {
+        "errors": [
+          {
+            "message": "Parse error on \"getPetById\" (IDENTIFIER) at [2, 3]",
+            "locations": [
+              {
+                "line": 2,
+                "column": 3
+              }
+            ]
+          }
+        ]
+      }
+    ).as('getAllPets');
+  });
+  
+  it('should display an error message when page is not found', () => {
     cy.visit('http://localhost:3000/error')
       .get('header')
-      .get('h1')
-      .contains('Error Message TBD')
+      .get('.error-message')
+      .contains('This page cannot be found.')
+      .get('.error-button')
+  })
+
+  it('should display a error message when an error occurs with the application', () => {
+    cy.visit('http://localhost:3000/')
+    cy.wait('@getAllPets')
+      .get('header')
+      .get('.error-message')
+      .contains('Something went wrong. Please reload the page and try again.')
+      .get('.error-button').click()
+    cy.location().should((loc) => {
+      expect(loc.href).to.eq('http://localhost:3000/');
+    });
+
   })
 })
