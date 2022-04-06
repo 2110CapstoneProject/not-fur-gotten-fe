d64b58dd357e5f17278597f78ac703b8076efec1
Christine Rowland <fordo29@gmail.com>
2022-04-05 19:37:04 -0600

    Add logic to show a message when there are no pets to adopt

--- src/Components/PetContainer.js
+++ src/Components/PetContainer.js
@@ -4,19 +4,27 @@ import '../Styles/PetContainer.scss';
 
 
 const PetContainer = ({ pets }) => {
-  const petCards = pets.map(pet => {
-    return (
-      <PetCard
-        id={pet.id}
-        name={pet.name}
-        image={pet.image}
-      />
-    )
-  });
+  let petCards;
+  let emptyMessage;
+
+  if(!pets.length){
+    emptyMessage = <p className='no-results'>No pets are currently up for adoption.</p>
+  } else {
+    petCards = pets.map(pet => {
+      return (
+        <PetCard
+          id={pet.id}
+          name={pet.name}
+          image={pet.image}
+        />
+      )
+    });
+  }
 
   return (
     <div className="pet-container-section">
       <h2 className="home-page-subheader">Pets Available For Adoption</h2>
+        {emptyMessage}
       <section className="pet-container">
         {petCards}
       </section>
